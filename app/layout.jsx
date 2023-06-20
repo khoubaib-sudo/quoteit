2276b125a56446aacad7e280f6537ecd9cb0e932
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-06-20 23:06:47 +0200

    home page

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "@styles/globals.css";
+export const meatdata = {
+  title: "Quoteit",
+  description: "Discover & Share Quotes",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
