8cbfa25251a4f4a948f6a6dd0a24a336daf93cfa
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-06-24 20:09:01 +0200

    Next.js API Routes

---
+++ app/update-quote/page.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
+
+
+const UpdateQuote = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const quoteId = searchParams.get("id");
+
+  const [post, setPost] = useState({ quote: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    const getQuoteDetails = async () => {
+      const response = await fetch(`/api/quote/${quoteId}`);
+      const data = await response.json();
+
+      setPost({
+        quote: data.quote,
+        tag: data.tag,
+      });
+    };
+
+    if (quoteId) getQuoteDetails();
+  }, [quoteId]);
+
+  const updateQuote = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    if (!quoteId) return alert("Missing QuoteId!");
+
+    try {
+      const response = await fetch(`/api/quote/${quoteId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          quote: post.quote,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updateQuote}
+    />
+  );
+};
+
+export default UpdateQuote;
